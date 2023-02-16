a74671c32706c1fd0886e388719d16c18f29a531
Alex <ianta@ualberta.ca>
2023-02-16 15:25:55 -0700

    displaying some basic timelines

---
+++ src/components/Timeline.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <span>{{ timeline.id }}</span>
+        <div class="d-flex justify-content-evenly timeline-container">
+            <div class="timeline-item flex-fill" 
+            v-for="entity in timeline.data">
+                {{ entity.symbol }}
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: 'Timeline',
+    props: ['timeline'],
+    data(){
+        return {
+            msg: "hello"
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+.timeline-container{
+    border: solid 2px #7a7a7a;
+}
+.timeline-item{
+    border-left: solid 2px #7a7a7a;
+    text-align: center;
+}
+
+.timeline-item:first-child{
+    border: none;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,27 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server:{
+    proxy:{
+      '/api': {
+        target: 'http://172.31.224.1:8080',
+        // localAddress: '0.0.0.0',
+        changeOrigin: true,
+        logLevel: 'debug',
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });}
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
