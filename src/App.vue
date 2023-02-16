609d7b73e15ab1a203ffbade9214a2a4843c397b
Alex <ianta@ualberta.ca>
2023-02-16 15:50:31 -0700

    added the json viewer for annotations

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Timeline from "./components/Timeline.vue";
+import AnnotationPane from "./components/AnnotationPane.vue"
 </script>
 
 <template>
   <div class="container-fluid">
     <div class="row">
       <div class="col">
+        <h1>Timelines</h1>
         <Timeline v-for="timeline in timelines"
           :key="timeline.id"
           :timeline="timeline"
+          @click="selectTimeline(timeline.id)"
         ></Timeline>
+      </div>
+      <div class="col">
+        <h1>Annotation</h1>
+        <AnnotationPane :timelineId="selected"></AnnotationPane>
       </div>
     </div>
   </div>
@@ -24,7 +31,8 @@ export default {
   name: 'App',
   data(){
     return{
-      timelines: []
+      timelines: [],
+      selected: undefined,
     }
   },
   mounted(){
@@ -34,7 +42,14 @@ export default {
       console.log(response.data[0])
       self.timelines = response.data
     })
+  },
+  methods:{
+    selectTimeline(id) {
+      this.selected = id
+    }
   }
+
+  
 }
 </script>
 
---
+++ src/components/AnnotationPane.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import JsonEditorVue from 'json-editor-vue'
+</script>
+
+<template>
+    <div class="h-100">
+        <JsonEditorVue v-model="value" class="h-100" ></JsonEditorVue>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'AnnotationPane',
+    props: ['timelineId'],
+    data(){
+        return{
+            value: '',
+        }
+    },
+    watch:{
+        timelineId: function(id){
+            let self = this
+            axios.get('/api/annotations/' + id + '/').then(function(response){
+                self.value = response.data
+            })
+        }
+    }
+}
+</script>
